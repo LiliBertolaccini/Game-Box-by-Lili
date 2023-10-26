3c4aad23c3336e95f61a37b22fa26c82f0ba1dc7
Lilibertola <bertolaccini55@gmail.com>
2023-10-25 21:24:39 -0300

    fet: home-scss

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import style from './home.module.scss';
+import React from "react";
+import style from "./home.module.scss";
 
-import Background from '../Components/Background';
+import Background from "../Components/Background";
 
-import coracao from '../Images/GBL-coracao-vida.webp';
-import avatar from '../Images/GBL-avatar.webp';
-import nuvemTriangulo from '../Images/GBL-nuvem-triangulo.webp';
-import arvoreBolas from '../Images/GBL-arvore-bolas.webp';
-import moeda from '../Images/GBL-moeda.webp';
-import terra from '../Images/GBL-terra.webp';
+import coracao from "../Images/GBL-coracao-vida.webp";
+import avatar from "../Images/GBL-avatar.webp";
+import nuvemTriangulo from "../Images/GBL-nuvem-triangulo.webp";
+import arvoreBolas from "../Images/GBL-arvore-bolas.webp";
+import moeda from "../Images/GBL-moeda.webp";
+import terra from "../Images/GBL-terra.webp";
+import trofeu from "../Images/GBL-trofeu.webp";
 
 function Home() {
   return (
     <div className={style.home_container}>
-      <h1>Game Box</h1>
-      <h2>by Lili</h2>
-      <img src={moeda} alt="moeda de pixel" className={style.home_moeda} />
-      <img src={arvoreBolas} alt="árvore de bolas de pixel" className={style.home_arvore} />
-      <img src={nuvemTriangulo} alt="nuvem em forma de triângulo de pixel" className={style.home_nuvem} />
-      <img src={coracao} alt="vidas de coração de pixel" className={style.home_coracao} />
+      <div className={style.home_el_topo_box}>
+        <div className={style.home_el_moeda}>
+          <img src={moeda} alt="moeda de pixel" className={style.home_moeda} />
+          <p>1250</p>
+        </div>
+        <img
+          src={coracao}
+          alt="vidas de coração de pixel"
+          className={style.home_coracao}
+        />
+      </div>
+      <div className={style.home_titulo_box}>
+        <img src={trofeu} alt="troféu de pixel" className={style.home_trofeu} />
+        <div className={style.home_titulo}>
+          <h1>Game Box</h1>
+          <h2>by Lili</h2>
+        </div>
+      </div>
+
+      <img
+        src={arvoreBolas}
+        alt="árvore de bolas de pixel"
+        className={style.home_arvore}
+      />
+      <img
+        src={nuvemTriangulo}
+        alt="nuvem em forma de triângulo de pixel"
+        className={style.home_nuvem}
+      />
       <img src={avatar} alt="avatar de pixel" className={style.home_avatar} />
       <img src={terra} alt="chão de pixel" className={style.home_terra} />
       <Background page="home" />
