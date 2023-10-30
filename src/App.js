5db4caed514b7c7c534775e7aa9a7fd166ad9a99
Lilibertola <bertolaccini55@gmail.com>
2023-10-29 22:03:28 -0300

    fet: iniciando componente game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import style from './app.module.scss';
 import Home from './Pages/Home';
+import Games from './Pages/Games';
+import arrow from './Images/GBL-seta.webp';
 
 function App() {
+  const [local, setLocal] = useState('games');
+  let componentPage ;
+  switch (local) {
+    case 'home':
+      componentPage = <Home />;
+      break;
+    case 'games':
+      componentPage = <Games />;
+      break;
+    default:
+      componentPage = '';
+      break;
+  }
+
+  useEffect(() => {
+
+      
+  }, [local])
+
   return (
     <div className={style.app_container}>
-      <Home />
+      {componentPage}
+      <Games />
     </div>
   );
 }
---
+++ src/Pages/Games.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./games.module.scss";
+
+function Games() {
+  return (
+    <div className={style.games_container}>
+      <h2>pag de escolher o jogo</h2>
+    </div>
+  );
+}
+
+export default Games;
