ea89a4d12d5316cb2c60fb6ab7b1cff4e5abb913
Lilibertola <bertolaccini55@gmail.com>
2023-10-31 17:02:10 -0300

    fet: add elem em home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -16,35 +16,61 @@ function Home() {
     <div className={style.home_container}>
       <div className={style.home_el_topo_box}>
         <div className={style.home_el_moeda}>
-          <img src={moeda} alt="moeda de pixel" className={style.home_moeda} />
+          <img
+          src={moeda}
+          alt="moeda de pixel"
+          className={style.home_moeda}
+          />
           <p>1250</p>
         </div>
+        <img
+        src={nuvemTriangulo}
+        alt="nuvem de pixel"
+        className={style.home_nuvemTriangulo}
+        />   
         <img
           src={coracao}
           alt="vidas de coração de pixel"
           className={style.home_coracao}
         />
+        <img
+      src={arvoreBolas}
+      alt="árvore de bolas de pixel"
+      className={style.home_arvoreBolas}
+        />
       </div>
       <div className={style.home_titulo_box}>
-        <img src={trofeu} alt="troféu de pixel" className={style.home_trofeu} />
+        <img
+        src={trofeu}
+        alt="troféu de pixel"
+        className={style.home_trofeu}
+        />
         <div className={style.home_titulo}>
           <h1>Game Box</h1>
           <h2>by Lili</h2>
         </div>
       </div>
-
       <img
+      src={avatar}
+      alt="avatar de pixel"
+      className={style.home_avatar}
+      />
+        <img
+        src={terra}
+        alt="chão de pixel"
+        className={style.home_terra}
+        />
+      {/*<img
         src={arvoreBolas}
         alt="árvore de bolas de pixel"
         className={style.home_arvore}
-      />
-      <img
+      />*/}
+      {/*<img
         src={nuvemTriangulo}
         alt="nuvem em forma de triângulo de pixel"
         className={style.home_nuvem}
-      />
-      <img src={avatar} alt="avatar de pixel" className={style.home_avatar} />
-      <img src={terra} alt="chão de pixel" className={style.home_terra} />
+      />*/}
+
       <Background page="home" />
     </div>
   );
