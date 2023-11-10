88fc44783f346cfe7602f75f83d4a62260035aed
Lilibertola <bertolaccini55@gmail.com>
2023-11-09 23:43:21 -0300

    feat: mais alterações incriveis

--- src/App.js
+++ src/App.js
@@ -1,59 +1,93 @@
-import { useState } from 'react';
-import './App.css';
-import style from './app.module.scss';
-import Home from './Pages/Home';
-import Games from './Pages/Games';
-import arrow from './Images/GBL-seta.webp';
-import GameDetails from './Pages/GameDetails';
-import ProjectDetails from './Pages/ProjectDetails';
-import Final from './Pages/Final';
-//import Jogo from './Pages/Jogo';
+import { useState } from "react";
+import "./App.css";
+import style from "./app.module.scss";
+import Home from "./Pages/Home";
+import Games from "./Pages/Games";
+import arrow from "./Images/GBL-seta.webp";
+import ProjectDetails from "./Pages/ProjectDetails";
+import Final from "./Pages/Final";
 
 function App() {
-  const [local, setLocal] = useState('games');
+  const [local, setLocal] = useState("home");
+  const [hasArrow, setHasArrow] = useState(true);
+
+//useEffect(() => {}); 
+
   const componentPage = () => {
     switch (local) {
-      case 'home':
-      return <Home />;
-    case 'games':
-      return <Games />;
-    case 'gameDetails':
-      return <GameDetails />;
-    case 'projectDetails':
-      return <ProjectDetails />;
-    //case 'jogo':
-      //return <Jogo />;
-      case 'final':
-      return <Final />;
-    default:
-      return null;
-  }
+      case "home":
+        return <Home />;
+      case "games":
+        return <Games />;
+      case "projectDetails":
+        return <ProjectDetails />;
+      case "final":
+        return <Final />;
+      default:
+        return null;
+    }
   };
 
-  const handleClick = (() => {
+  const handleClick = () => {
     switch (local) {
-      case 'home':
-        return setLocal('games');
-      case 'games':
-        return setLocal('gameDetails');
-      case 'gameDetails':
-        return setLocal('projectDetails');
-      case 'projectDetails':
-        return setLocal('final');
-      case 'final':
-        return setLocal('home');
-    default:
-      return null;
+      case "home":
+        setLocal("games");
+        setHasArrow(true);
+        break;
+      case "games":
+        setLocal("projectDetails");
+        setHasArrow(true);
+        break;
+      case "projectDetails":
+        setLocal("final");
+        setHasArrow(false);
+        break;
+      default:
+        return null;
     }
-  });
+  };
+
+  const handleClickLeft = () => {
+    switch (local) {
+      case "home":
+        break;
+      case "games":
+        setLocal("home");
+        setHasArrow(true);
+        break;
+      case "projectDetails":
+        setLocal("games");
+        setHasArrow(true);
+        break;
+      case "final":
+        setLocal("projectDetails");
+        setHasArrow(true);
+        break;
+      default:
+        return null;
+    }
+  };
 
   return (
-    <div className={style.app_container}>
-      <img src={arrow} alt='arrow' className={`${style.app_arrow} ${style.app_arrow_left}`} onClick={handleClick}/>
+    <main className={style.app_container}>
+      <img
+        src={arrow}
+        alt="arrow"
+        className={style.app_arrow}
+        style={local === 'home' ? { visibility: 'hidden' } : {}}
+        onClick={handleClickLeft}
+      />
 
-      { componentPage() }
-      <img src={arrow} alt='arrow' className={style.app_arrow} onClick={handleClick}/>
-    </div>
+      {componentPage()}
+      {hasArrow && (
+        <img
+          src={arrow}
+          alt="arrow"
+          className={style.app_arrow}
+          onClick={handleClick}
+        />
+      )}
+    </main>
   );
 }
 
